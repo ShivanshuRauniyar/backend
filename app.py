f12e38b09630667810a18eaad7889b1283545959
ShivanshuRauniyar <100207063+ShivanshuRauniyar@users.noreply.github.com>
2025-11-02 13:59:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,9 @@ RAZORPAY_KEY_ID = "rzp_test_RabCipwRqE6OvD"
 RAZORPAY_KEY_SECRET = "CRq63kgfFXVlnnG7dnxjhMzF"
 
 razorpay_client = razorpay.Client(auth=(RAZORPAY_KEY_ID, RAZORPAY_KEY_SECRET))
-
+@app.route('/')
+def index():
+    return "Flask backend is live 🚀"
 # ---------- USER AUTH (REGISTER / LOGIN) ----------
 @app.route("/register", methods=["POST"])
 def register_user():
